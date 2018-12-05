fa8afb724749f5e3b9e0ce253875c46e001fd696
Victoria Godfrey <victorialgodfrey@gmail.com>
2018-12-04 22:15:23 -0500

    a11y edits

---
+++ index.js
@@ -0,0 +1,75 @@
+let questionNumber = 1;
+let score = 0;
+
+//Establish questions + answers
+
+
+//generate Question
+function generateQuestion(){
+
+}
+
+//add one to question number
+function changeQuestionNumber(){
+
+}
+
+
+//add one to score
+function changeScore () {
+
+}
+
+//update score
+function updateScore(){
+	
+}
+
+
+//start quiz
+function startQuiz(){
+
+}
+
+//render question 
+function renderQuestion(){
+
+}
+
+//choose answer
+
+function chooseAnswer(){
+
+}
+
+//correct answer feedback
+function correctAnswer(){
+
+}
+
+//incorrect answer
+function incorrectAnswer(){
+
+}
+
+
+//Quiz finished
+function results(){
+
+}
+
+//next
+function nextQuestion(){
+
+}
+
+//start the quiz 
+function restartQuiz(){
+
+}
+
+
+//run quiz
+function runQuiz(){
+
+}
