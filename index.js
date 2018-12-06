58fffe9863cbe891c1478f1da10035b1e09a4d9c
Victoria Godfrey <victorialgodfrey@gmail.com>
2018-12-06 08:20:00 -0500

    CSS styling updates and JS start

--- index.js
+++ index.js
@@ -4,39 +4,86 @@ let score = 0;
 
 
 //Establish questions + answers
+const quizQuestions = [
+{
+	question: "What is Nearly Headless Nick’s Full Name?",
+	answers: {
+		'Sir Nicholas de Monsky-Pimpton',
+		'Sir Nicholas of Minsky-Popington',
+		'Sir Nicholas de Mimsy-Porpington',
+		'Sir Nicholas of Mimsy-Popington'
+	}, 
+	correctAnswer: 'Sir Nicholas de Mimsy-Porpington'
+}
+];
 
 
-//generate Question
+	
+
+//generate Question w/ HTML
 function generateQuestion(){
+	if (questionNumber < quizQuestions.length) {
+		return `<div class ="question">
+				<fieldset>
+				<legend class ="question-text"> ${quizQuestions.question} </legend>
+				<form>
+						<label class = "answer-choice">
+							<input type = "radio" value="choicetext" name = "answer" class="answer" required>
+							<span> ${quizQuestions.answers[0]} </span>
+						</label>
+						<label class = "answer-choice">
+							<input type = "radio" value="choicetext" name = "answer" class="answer" required>
+							<span> ${quizQuestions.answers[1]} </span>
+						</label>
+						<label class = "answer-choice">
+							<input type = "radio" value="choicetext" name = "answer" class="answer" required>
+							<span>${quizQuestions.answers[2]} </span>
+						</label>
+						</label>
+						<label class = "answer-choice">
+							<input type = "radio" value="choicetext" name = "answer" class="answer" required>
+							<span> ${quizQuestion.answers[3]} </span>
+						<button type="submit" class="submit-button"> submit</button>
+					</fieldset>
+				</form>
+			</div>`
+	} else {
+		//render results
+		//restart quiz
+	}
 
 }
 
 //add one to question number
 function changeQuestionNumber(){
+	questionNumber += 1;
+	return questionNumber;
 
 }
 
 
 //add one to score
 function changeScore () {
-
+	score += 1;
+	return score;
 }
 
 //update score
-function updateScore(){
-	
-}
+//function updateScore(){
+	//this would change how the score displays in DOM
+//}
 
 
 //start quiz
-
 function startQuiz(){
-	
-}
+	//click start botton
+    //displays the quiz
+};
 
 //render question 
-function renderQuestion(){
-
+function displayQuestion(){
+	//display rendered question
+	//why cant I just display directly
 }
 
 //choose answer
@@ -49,6 +96,7 @@ function chooseAnswer(){
 //correct answer feedback
 function correctAnswer(){
 	//return 'correct'
+	//display correct 
 
 }
 
@@ -61,6 +109,7 @@ function incorrectAnswer(){
 
 //Quiz finished
 function results(){
+	//display final score
 
 }
 
@@ -71,6 +120,7 @@ function nextQuestion(){
 
 //start the quiz 
 function restartQuiz(){
+	//this will be a button to start the quiz at question 1/start page 
 
 }
 
